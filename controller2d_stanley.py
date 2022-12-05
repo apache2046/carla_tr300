fc0af0cf1dd36e77bd6c89a7bca315ad2a1c7807
apache2046 <apache2046@gmail.com>
2022-12-05 18:48:23 +0800

    1

--- controller2d_stanley.py
+++ controller2d_stanley.py
@@ -134,12 +134,12 @@ class Controller2D(object):
         front_center_y = y + np.sin(yaw) * L / 2
         
         K_v = 0.5
-        ld = max(3*L, K_v*v)
+        #ld = max(3*L, K_v*v)
 
         wpt = waypoints.copy()
         wpt[:, 0] -= front_center_x
         wpt[:, 1] -= front_center_y
-        dist = np.abs(wpt[:, 0]**2 + wpt[:, 1] ** 2 - ld ** 2)
+        dist = np.abs(wpt[:, 0]**2 + wpt[:, 1] ** 2)# - ld ** 2)
         nearest_idx = dist.argmin()
         if nearest_idx == len(waypoints) -1:
             nearest_idx -= 1
@@ -162,9 +162,9 @@ class Controller2D(object):
         elif psai < -np.pi:
             psai += 2 * np.pi
 
-        Ks = 4
-        Ke = 0.5
-        steer_output = 0.5 * psai + np.arctan2(Ke * cross_track_error, Ks + v)
+        Ks = 8 #4
+        Ke = 10.0  #7.0 # 0.5
+        steer_output = psai + np.arctan2(Ke * cross_track_error, Ks + v)
 
         ######################################################
         # SET CONTROLS OUTPUT
